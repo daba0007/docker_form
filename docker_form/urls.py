e081850909c255e563c9a4408474b55c0841cfa3
daba0007 <yld060631@163.com>
2018-01-31 21:15:01 +0800

    2018年1月31日21点14分

--- docker_form/urls.py
+++ docker_form/urls.py
@@ -6,11 +6,7 @@ from visual import views as visual_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home$', visual_views.home, name='home'),
-<<<<<<< HEAD
-=======
     url(r'^$', visual_views.home, name='home'),
-
->>>>>>> 5b473a3b4defa7d2db70b799bf8bee179e63772f
     url(r'',include('visual.urls')),
     url(r'',include('connect.urls')),
     url(r'',include('user.urls')),
